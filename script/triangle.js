5bf972c15a0243fa440a132e82bd0e70801ce0aa
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-26 11:01:39 +0600

    Get Triangle Base and Height using event and function

---
+++ script/triangle.js
@@ -0,0 +1,17 @@
+/**
+ * Objective: get base, height of a triangle. Calculate the area by using the provided formula. and then display the area.
+ * step -1: get base value of the triangle
+ * step-2: added an id in the base input field
+ * step-3: use getElementById to access the input field
+ * step-4: get value from the input field. (value is string now)
+ * step-5: convert the value to a number. use parseFloat
+ * 
+ */
+
+
+function calculateTriangleArea(){
+    const triangleBaseInput = document.getElementById('triangle-base');
+    const triangleBaseText = triangleBaseInput.value;
+    const triangleBase = parseFloat(triangleBaseText);
+    console.log(triangleBase);
+}
