923c9fe6e7401cc7a8717b01bc160946e9a6b1fe
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-27 23:51:37 +0600

    Rectangle card design

---
+++ script/rectangle.js
@@ -0,0 +1,8 @@
+function calculateRectangleArea() {
+
+    //  get length
+    const rectangleLengthInput = document.getElementById('rectangle-length');
+    const rectangleLengthText = rectangleLengthInput.value;
+    const rectangleLength = parseFloat(rectangleLengthText);
+    console.log(typeof rectangleLength);
+}
