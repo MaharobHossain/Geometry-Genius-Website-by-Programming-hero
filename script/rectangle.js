86a68381068ff96fc6c40934235dc6b1dbc9c672
Maharob Hossain <146083736+MaharobHossain@users.noreply.github.com>
2025-03-28 12:33:14 +0600

    Calculate Rectangle area and display

--- script/rectangle.js
+++ script/rectangle.js
@@ -1,8 +1,22 @@
 function calculateRectangleArea() {
 
-    //  get length
+    //  get length of the Rectangle
     const rectangleLengthInput = document.getElementById('rectangle-length');
     const rectangleLengthText = rectangleLengthInput.value;
     const rectangleLength = parseFloat(rectangleLengthText);
-    console.log(typeof rectangleLength);
+    console.log(rectangleLength);
+
+    //  get width of the Rectangle
+    const rectangleWidthInput = document.getElementById('rectangle-width');
+    const rectangWidthText = rectangleWidthInput.value;
+    const rectangWidth = parseFloat(rectangWidthText);
+    console.log(rectangWidth);
+
+    // Calculate Rectangle Area
+    const area = rectangleLength * rectangWidth;
+    console.log("Area of the rectengle is: ", area);
+
+    // Display Rectangle Area
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
 }
